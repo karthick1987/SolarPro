c21a30bc2fcaa6eadb9104e140bf6ace9a2732ab
Johannes Machleid <johannes.machleid@tum.de>
2019-05-29 16:39:20 +0200

    Solved Lesson 4

--- Lab_exercises/4-Lesson/L4_External_Sensors/external_sensors.c
+++ Lab_exercises/4-Lesson/L4_External_Sensors/external_sensors.c
@@ -1,3 +1,4 @@
+
 /*
    Wireless Sensor Networks Laboratory
 
@@ -42,6 +43,7 @@
 
 // Reading frequency in seconds.
 #define TEMP_READ_INTERVAL CLOCK_SECOND*1
+#define JOYSTICK_POLLING CLOCK_SECOND/100 //10ms
 
 /*** CONNECTION DEFINITION***/
 
@@ -79,24 +81,113 @@ static const struct broadcast_callbacks broadcast_callbacks = {broadcast_recv};
 //@param b: calibration value b inscribed on the back of the sensor
 //@param adc_input: phidget input value. Use ZOUL_SENSORS_ADC1 or ZOUL_SENSORS_ADC3 depending on where the sensor is connected to.
 //@return int : lux value with a max of 1000.
-static int getLightSensorValue(float m, float b, uint8_t adc_input){
+static int getLightSensorValue(uint16_t adc_value){
 	//Read voltage from the phidget interface
-	double sensorValue = adc_input/4.096;
+	double sensorValue = adc_value/4.096;
+
 	//Convert the voltage in lux with the provided formula
-	double luxRaw = m * sensorValue + b;
+	double luxRaw = 1.4761 * sensorValue + 39.416;
+
 	//Return the value of the light with maximum value equal to 1000
-	uint8_t lux = luxRaw;
+	uint16_t lux = luxRaw;
+
 	if (lux > 1000){
 		lux = 1000;
 	}
 	return lux;
 }
 
+const char * getJoystickPosition(uint16_t x, uint16_t y){
+	// return values can be interpreted as followed:
+
+	if (x<300){
+		return "UP";
+	}
+	else if(x>2000){
+		return "DOWN";
+	}
+	else if(y<300){
+		return "LEFT";
+	}
+	else if(y>1500){
+		return "RIGHT";
+	}
+	else{
+		return "IDLE";
+	}
+
+}
+
 //--------------------- PROCESS CONTROL BLOCK ---------------------
 PROCESS (ext_sensors_process, "External Sensors process");
-AUTOSTART_PROCESSES (&ext_sensors_process);
+PROCESS (ext_sensors_joystick_process, "External Sensors Joystick process");
+AUTOSTART_PROCESSES (&ext_sensors_process, &ext_sensors_joystick_process);
 
-//------------------------ PROCESS' THREAD ------------------------
+
+//------------------------ PROCESS' EXTERNAL JOYSTICK THREAD ------------------------
+PROCESS_THREAD (ext_sensors_joystick_process, ev, data){
+	static struct etimer joystick_reading_timer;
+	const char * direction;
+	static uint16_t adc1_value, adc3_value;
+
+	PROCESS_BEGIN();
+
+	/*
+	 * set your group's channel
+	 */
+	NETSTACK_CONF_RADIO.set_value(RADIO_PARAM_CHANNEL,11);
+
+	/*
+	 * open the connection
+	 */
+	broadcast_open(&broadcastConn,129,&broadcast_callbacks);
+
+
+	/* Configure the ADC ports */
+	adc_zoul.configure(SENSORS_HW_INIT, ZOUL_SENSORS_ADC1 | ZOUL_SENSORS_ADC3);
+
+
+	etimer_set(&joystick_reading_timer, JOYSTICK_POLLING);
+
+	while(1){
+
+		PROCESS_WAIT_EVENT();
+
+		if(ev == PROCESS_EVENT_TIMER) {
+
+
+			/*
+			 * Read ADC values. Data is in the 12 MSBs
+			 */
+			adc1_value = adc_zoul.value(ZOUL_SENSORS_ADC1) >> 4;
+			adc3_value = adc_zoul.value(ZOUL_SENSORS_ADC3) >> 4;
+			direction = getJoystickPosition(adc1_value, adc3_value);
+
+			if (direction != "IDLE"){
+			/*
+			 * fill the packet buffer
+			 */
+			leds_on(LEDS_GREEN);
+			packetbuf_copyfrom(direction,10);
+
+			/*
+			 * send the message
+			 */
+			broadcast_send(&broadcastConn);
+
+			leds_off(LEDS_GREEN);
+			}
+
+			etimer_set(&joystick_reading_timer, JOYSTICK_POLLING);
+		}
+	}
+
+	PROCESS_END();
+
+}
+
+
+//------------------------ PROCESS' EXTERNAL SENSORS THREAD ------------------------
 PROCESS_THREAD (ext_sensors_process, ev, data) {
 
 	/* variables to be used */
@@ -129,8 +220,7 @@ PROCESS_THREAD (ext_sensors_process, ev, data) {
 	while (1) {
 
 		PROCESS_WAIT_EVENT();  // let process continue
-
-		/* If timer expired, pront sensor readings */
+		/* If timer expired, print sensor readings */
 	    if(ev == PROCESS_EVENT_TIMER) {
 
 	    	leds_on(LEDS_PURPLE);
@@ -148,8 +238,10 @@ PROCESS_THREAD (ext_sensors_process, ev, data) {
 	    	printf("\r\nADC1 value [Raw] = %d", adc1_value);
 	        printf("\r\nADC3 value [Raw] = %d", adc3_value);
 
-	        int light_value = getLightSensorValue(1.4761,39.416,adc1_value);
-	        printf("\r\nLuminosity is %d LUX", light_value);
+	        //uint16_t light_value1 = getLightSensorValue(adc1_value);
+	        //uint16_t light_value3 = getLightSensorValue(adc3_value);
+	        //printf("\r\nLuminosity via ADC1 is %d LUX", light_value1);
+	        //printf("\r\nLuminosity via ADC3 is %d LUX", light_value3);
 
 
     		leds_off(LEDS_PURPLE);
